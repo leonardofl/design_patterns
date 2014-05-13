fb16f70d71cf4681c900ea86e7e06705b107daa2
leonardofl <leonardofl87@gmail.com>
2014-05-12 22:13:14 -0300

    montando apresentação builder

--- builder/invoker.py
+++ builder/invoker.py
@@ -1,5 +1,42 @@
 
-# Motivação inicial do Builder: evitar a explosão de construtores
+
+
+# Builder
+# Encapsula o processo de criação de objetos complexos. [HEAD FIRST]
+# Possibilita a criação de objetos em um processo variável, flexível e multi-passo. [HEAD FIRST]
+# Possibilita a utilização de interface fluente na criação de objeto. [GUERRA]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Cenário específico: criação de objetos com argumentos opcionais.
+
+
+
 
 class Invoker(object):
   # Invoker original: 
@@ -8,11 +45,11 @@ class Invoker(object):
   def __init__(self, taskName, task, timeout, trials, pause, time_unit):
     """
       Argumentos:
-        taskName - nome da tarefa -- string
-        task - tarefa a ser executada -- uma função
-        timeout - tempo limite para uma tentatvia em segundos -- int
-        trials - quantidade de tentativas em segundos -- int
-        pause - pausa entre as tentativas em segundos -- int
+        taskName - nome da tarefa 
+        task - função a ser executada
+        timeout - tempo limite para uma tentativa, em segundos -- int
+        trials - quantidade de tentativas, em segundos -- int
+        pause - pausa entre as tentativas, em segundos -- int
         time_unit - string \in {'sec', 'min'}
     """
     self.taskName = taskName
@@ -34,6 +71,135 @@ class Invoker(object):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# Solução 0
+# Explosão de __init__'s.
+
+  def __init__(self, taskName, task, timeout):
+  def __init__(self, taskName, task, timeout, trials):
+  def __init__(self, taskName, task, timeout, trials, pause):
+  def __init__(self, taskName, task, timeout, time_unit):
+  def __init__(self, taskName, task, timeout, trials, time_unit):
+  def __init__(self, taskName, task, timeout, trials, pause, time_unit):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solução 1
+# Jogar no __init__ só o que for obrigatório, e settar o resto linha a linha
+
+class Invoker:
+
+  DEFAULT_TRIALS = 1
+  DEFAULT_PAUSE = 0
+  DEFAULT_TIME_UNIT = 'sec'
+
+  def __init__(self, taskName, task, timeout):
+    self.taskName = taskName
+    self.task = task
+    self.timeout = timeout
+    self.trials = DEFAULT_TRIALS
+    self.pause = DEFAULT_PAUSE
+    self.time_unit = DEFAULT_TIME_UNIT
+
+  def invoke(self):
+    # faz de conta que tá usando timeout, trials, pause e time_unit
+    try:
+      return self.task()
+    except:
+      raise RuntimeError('%s falhou' % taskName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Código cliente
+
+task = lambda : 5*4*3*2
+invoker = InvokerBuilder("fatorial", task, 1)
+invoker.trials = 3
+invoker.pause = 1 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Considerações
+# muito feio, neh?
+# mais algum problema?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solução 2
+# Traduzindo o Builder feito no Java
+
+
 class InvokerBuilder(object):
   # InvokerBuilder original:
   # https://github.com/choreos/enactment_engine/blob/master/Commons/src/main/java/org/ow2/choreos/invoker/InvokerBuilder.java
@@ -42,7 +208,7 @@ class InvokerBuilder(object):
   DEFAULT_PAUSE = 0
   DEFAULT_TIME_UNIT = 'sec'
 
-  def __init__(self, taskName, task, timeout, time_unit):
+  def __init__(self, taskName, task, timeout):
     self.taskName = taskName
     self.task = task
     self.timeout = timeout
@@ -63,32 +229,90 @@ class InvokerBuilder(object):
     return self
 
   def build(self):
-    return Invoker(self.taskName, self.task, self.timeout, self.trials, self.pause)
+    return Invoker(self.taskName, self.task, self.timeout, self.trials, self.pause, self.time_unit)
 
 
 
-# usando o builder, ficaria assim
-f = lambda x : x*x
-invoker = InvokerBuilder("quadrado", f, 1).trials(3).pause(1).time_unit('min').build()
-
-# funciona, mas... me parece q não ficou mt pythonico, neh?
-
-######################################
 
 
-# Uma primeira tentativa um **pouco** mais pythonica 
+
+
+
+
+
+
+
+
+
+
+
+
+# Código cliente
+
+task = lambda : 5*4*3*2
+invoker = InvokerBuilder("fatorial", task, 1).trials(3).pause(1).time_unit('min').build()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Considerações
+# funciona, mas... será que isso é pythonico?
+# aliás, interface fluente faz sentido em Python?
+# Mas vamos ver outras alternativas!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solução 3
+# Argumentos opcionais
+
+
+
 class Invoker(object):
-  # Invoker original: 
-  # https://github.com/choreos/enactment_engine/blob/master/Commons/src/main/java/org/ow2/choreos/invoker/DefaultInvoker.java  
 
   def __init__(self, taskName, task, timeout, trials=1, pause=0, time_unit='sec'):
     """
       Argumentos:
-        taskName - nome da tarefa -- string
-        task - tarefa a ser executada -- uma função
-        timeout - tempo limite para uma tentatvia em segundos -- int
-        trials - quantidade de tentativas em segundos -- int (padrão é 1)
-        pause - pausa entre as tentativas em segundos -- int (padrão é 0)
+        taskName - nome da tarefa 
+        task - função a ser executada
+        timeout - tempo limite para uma tentativa, em segundos -- int
+        trials - quantidade de tentativas, em segundos -- int (padrão é 1)
+        pause - pausa entre as tentativas, em segundos -- int (padrão é 0)
         time_unit - string \in {'sec', 'min'} (padrão é 'sec')
     """
     self.taskName = taskName
@@ -105,23 +329,69 @@ class Invoker(object):
     except:
       raise RuntimeError('%s falhou' % taskName)
 
-# Problema: E se eu quiser especificar pause, mas não trials?
-#(bom, se não fosse o time_unit até q essa versão resolvia o problema, pois não faz sentido especificar pause se trials=1)
-
-# Solução 1: jogar no init só oq é obrigatório, e settar o resto linha a linha
-
-f = lambda x : x*x
-invoker = Invoker("quadrado", f, 100)
-invoker.trials = 3
-invoker.pause = 1 
-# mt feio, neh?
 
 
-# Solução 2: kwargs!
-# TODO implementar
-# mostrar exemplo de kargs da query do django
-# draw-back: perda de expressividade; documentação fica mais complicada
-# e como fica a introspecção?
+
+
+
+
+
+
+
+
+
+
+
+
+# Código cliente
+
+task = lambda : 5*4*3*2
+invoker = InvokerBuilder("fatorial", task, 100, 2, 10)
+invoker = InvokerBuilder("fatorial", task, timeout=100, trials=2, pause=10)
+invoker = InvokerBuilder("fatorial", task, timeout=1, time_unit='min')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Considerações
+# Builder sumiu!
+# Seria essa solução mais pythonica?
+# Poder fazer "trials=2" é um recurso muito legal do python!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solução 4
+# kwargs!
 
 class Invoker(object):
 
@@ -155,22 +425,127 @@ class Invoker(object):
       raise RuntimeError('%s falhou' % taskName)
 
 
-# Usando Invoker criado com kwargs:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Código cliente
 invoker = Invoker("quadrado", f, 100, trials=3, pause=2)
 
-# Até agora consideramos o Builder como uma forma de "instanciar o objeto de uma vez"
-# mas ele tb possui uma outra motivação mais profunda, ele ajuda a criar objetos imutáveis.
-# Objetos imutáveis tem suas vantagens [ref effective Java]
-# Vide programação funcional: totalmente baseada na imutabilidade dos objetos
-# Imutabilidade favorece concorrência
-# Mas...... considerando o não-encapsulamento do python, faz sentido usar o Builder para esse propósito?
-# faz sentido se preocupar com imutabilidade de objetos no Python?
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Considerações
+# Perda de expressividade
+# documentação fica mais complicada (?)
+# Parece não ter vantagem sobre os argumentos opcionais
+
+
+
+
+
+
+
+
+
+
+
+# TODO mostrar exemplo de kargs da query do django
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Considerações finais
+
+# Até agora consideramos o Builder como uma forma de "instanciar o objeto de uma vez".
+# Mas ele também possui uma outra motivação mais profunda, ele ajuda a criar objetos imutáveis.
+# Objetos imutáveis tem suas vantagens [EFFECTIVE JAVA]:
+#   Vide programação funcional: totalmente baseada na imutabilidade dos objetos.
+#   Imutabilidade favorece concorrência.
+# Mas...... considerando o "não-encapsulamento" do Python, faz sentido usar o Builder para esse propósito?
+# Faz sentido se preocupar com imutabilidade de objetos no Python?
+# TODO encontrar exemplo em que estado inicial do objeto seria inválido
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Bibliografia
+# [HEAD FIRST] Eric Freeman & Elisabeth Freeman, Cap 14. Appendix: Leftover Pattern. Em: Head First Design Patterns. O'Reilly. 2004
+# [GUERRA] Eduardo Guerra, Cap 6. Estratégias de Criação de Objetos. Em: "Design Patterns com Java. Projeto Orientado a Objetos guiado por Padrões". Casa do Código. 2013
+# [EFFECTIVE JAVA]
 
 
 
