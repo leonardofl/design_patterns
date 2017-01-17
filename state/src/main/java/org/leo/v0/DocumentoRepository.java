b1bee8fe24c30e0b993775a2c46a873497fa301d
Leonardo Leite <leonardofl87@gmail.com>
2017-01-17 15:38:03 -0200

    state v0: transições inválidas não são protegidas.

---
+++ state/src/main/java/org/leo/v0/DocumentoRepository.java
@@ -0,0 +1,31 @@
+package org.leo.v0;
+
+public class DocumentoRepository {
+
+	private DocumentoDAO documentoDAO = new DocumentoDAO();
+	
+	public void emitir(Documento documento) {
+		documento.setEstado(EstadoDocumento.EMITIDO);
+		documentoDAO.inserir(documento);
+	}
+
+	public void alterar(Documento documento) {
+		documento.setEstado(EstadoDocumento.EMITIDO);
+		documentoDAO.alterar(documento);
+	}
+
+	public void cancelar(Documento documento) {
+		documento.setEstado(EstadoDocumento.CANCELADO);
+		documentoDAO.alterar(documento);
+	}
+	
+	public void consumir(Documento documento) {
+		documento.setEstado(EstadoDocumento.CONSUMIDO);
+		documentoDAO.alterar(documento);
+	}
+	
+	public Documento consultar(String numeroDocumento) {
+		return documentoDAO.consultar(numeroDocumento);
+	}
+
+}
---
+++ state/src/main/java/org/leo/v0/EstadoDocumento.java
@@ -0,0 +1,9 @@
+package org.leo.v0;
+
+public enum EstadoDocumento {
+	
+	EMITIDO,
+	CANCELADO,
+	CONSUMIDO;
+	
+}
